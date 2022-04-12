4e38d2777fceab5f416671a90e60022ecff546f1
liuyunan <liuyunan.20@gmail.com>
2022-04-12 00:37:07 -0700

    it returns the result of math.sqrt() for integers and floats, the string "The string should be converted into a numeric data type" if the passed argument was a string, and None for other inputs.

---
+++ Topics/Experiments with Python shell/Creating a function/main.py
@@ -0,0 +1,10 @@
+import math
+
+
+# Modify this function in the shell and copy the new version here
+def my_sqrt(value):
+    if type(value) is int or type(value) is float:
+        return math.sqrt(value)
+    if type(value) is str:
+        return "The string should be converted into a numeric data type"
+    return None
