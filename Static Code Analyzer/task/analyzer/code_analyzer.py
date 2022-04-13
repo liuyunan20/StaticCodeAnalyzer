6e43b1d384fb83c2f795949ed59bc995dedf0192
liuyunan <liuyunan.20@gmail.com>
2022-04-13 15:04:50 -0700

    In this stage, you need to add checks for the following five errors to your program:
    [S002] Indentation is not a multiple of four;
    
    [S003] Unnecessary semicolon after a statement (note that semicolons are acceptable in comments);
    
    [S004] Less than two spaces before inline comments;
    
    [S005] TODO found (in comments only and case-insensitive);
    
    [S006] More than two blank lines preceding a code line (applies to the first non-empty line).
    
    Please note that:
    
    If a line contains the same stylistic issue several times, your program should print the information only once. However, if a single line has several issues with different types of error codes, they should be printed as a sorted list.
    
    To simplify the task, we consider it acceptable if your program finds some false-positive stylistic issues in strings, especially in multi-lines ('''...''' and """...""").
    
    We recommend that you break your code into a set of functions to avoid confusion.

--- Static Code Analyzer/task/analyzer/code_analyzer.py
+++ Static Code Analyzer/task/analyzer/code_analyzer.py
@@ -1,7 +1,29 @@
+import re
+
+
+def check_error(code, num, blank_line):
+    if len(code) > 79:
+        print(f"Line {num}: S001 Too long")
+    if code.strip("\n") != "" and (len(code) - len(code.lstrip())) % 4 != 0:
+        print(f"Line {num}: S002 Indentation is not a multiple of four")
+    if ";" in code and not re.search(r'#.*;', code) and not re.search(r"['\"].*;.*['\"]", code):
+        print(f"Line {num}: S003 Unnecessary semicolon")
+    if "#" in code and not code.startswith("#") and not re.match(r'.+\s{2,}#.*', code):
+        print(f"Line {num}: S004 At least two spaces required before inline comments")
+    if re.search(r'#.*[Tt][Oo][Dd][Oo].*', code):
+        print(f"Line {num}: S005 TODO found")
+    if code.strip("\n") != "" and blank_line > 2:
+        print(f"Line {num}: S006 More than two blank lines used before this line")
+
+
 file_path = input()
 with open(file_path, 'r') as code_file:
     line_num = 0
+    blank_lines = 0
     for line in code_file:
         line_num += 1
-        if len(line) > 79:
-            print(f"Line {line_num}: S001 Too long")
+        if line.strip("\n") == "":
+            blank_lines += 1
+        else:
+            check_error(line, line_num, blank_lines)
+            blank_lines = 0
