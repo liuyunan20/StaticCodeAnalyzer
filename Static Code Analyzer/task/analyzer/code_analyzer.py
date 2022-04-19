409fa48da847023c66528ff890dba47b60607cbe
liuyunan <liuyunan.20@gmail.com>
2022-04-19 01:28:51 -0700

    As a rule, real projects contain more than a single file. Also, project directories often contain not only Python files, and we don't need to check if an HTML file follows PEP8. In this stage, you need to improve your program so that it can analyze all Python files inside a specified directory.

--- Static Code Analyzer/task/analyzer/code_analyzer.py
+++ Static Code Analyzer/task/analyzer/code_analyzer.py
@@ -1,29 +1,43 @@
 import re
+import sys
+import os
 
 
-def check_error(code, num, blank_line):
+def check_error(path, code, num, blank_line):
     if len(code) > 79:
-        print(f"Line {num}: S001 Too long")
+        print(f"{path}: Line {num}: S001 Too long")
     if code.strip("\n") != "" and (len(code) - len(code.lstrip())) % 4 != 0:
-        print(f"Line {num}: S002 Indentation is not a multiple of four")
+        print(f"{path}: Line {num}: S002 Indentation is not a multiple of four")
     if ";" in code and not re.search(r'#.*;', code) and not re.search(r"['\"].*;.*['\"]", code):
-        print(f"Line {num}: S003 Unnecessary semicolon")
+        print(f"{path}: Line {num}: S003 Unnecessary semicolon")
     if "#" in code and not code.startswith("#") and not re.match(r'.+\s{2,}#.*', code):
-        print(f"Line {num}: S004 At least two spaces required before inline comments")
+        print(f"{path}: Line {num}: S004 At least two spaces required before inline comments")
     if re.search(r'#.*[Tt][Oo][Dd][Oo].*', code):
-        print(f"Line {num}: S005 TODO found")
+        print(f"{path}: Line {num}: S005 TODO found")
     if code.strip("\n") != "" and blank_line > 2:
-        print(f"Line {num}: S006 More than two blank lines used before this line")
+        print(f"{path}: Line {num}: S006 More than two blank lines used before this line")
 
 
-file_path = input()
-with open(file_path, 'r') as code_file:
-    line_num = 0
-    blank_lines = 0
-    for line in code_file:
-        line_num += 1
-        if line.strip("\n") == "":
-            blank_lines += 1
-        else:
-            check_error(line, line_num, blank_lines)
-            blank_lines = 0
+def get_path():
+    base_path = sys.argv[1]
+    paths = []
+    if not base_path.endswith(".py"):
+        for entry in os.listdir(base_path):
+            if entry.endswith(".py"):
+                paths.append(os.path.join(base_path, entry))
+    else:
+        paths.append(base_path)
+    return sorted(paths)
+
+
+for file_path in get_path():
+    with open(file_path, 'r') as code_file:
+        line_num = 0
+        blank_lines = 0
+        for line in code_file:
+            line_num += 1
+            if line.strip("\n") == "":
+                blank_lines += 1
+            else:
+                check_error(file_path, line, line_num, blank_lines)
+                blank_lines = 0
