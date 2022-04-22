29878e810e683213b5b19345c1cce3504c7874f9
liuyunan <liuyunan.20@gmail.com>
2022-04-22 10:41:10 -0700

    In this final stage, you need to improve your program to check that all the names of function arguments as well as local variables meet the requirements of PEP8. The program must not force the names of variables outside of functions (for example, in modules or classes). The most convenient way to do this is to use the Abstract Syntactic Tree (AST) from the ast module.
    Also, your program must check that the given code does not use mutable values (lists, dictionaries, and sets) as default arguments to avoid errors in the program.
    
    Objectives
    You need to add three new checks to your analyzer:
    
    [S010] Argument name arg_name should be written in snake_case;
    
    [S011] Variable var_name should be written in snake_case;
    
    [S012] The default argument value is mutable.

--- Static Code Analyzer/task/analyzer/code_analyzer.py
+++ Static Code Analyzer/task/analyzer/code_analyzer.py
@@ -1,9 +1,10 @@
 import re
 import sys
 import os
+import ast
 
 
-def check_error(path, code, num, blank_line):
+def check_error(path, code, num, blank_line, tree_result):
     if len(code) > 79:
         print(f"{path}: Line {num}: S001 Too long")
     if code.strip("\n") != "" and (len(code) - len(code.lstrip())) % 4 != 0:
@@ -19,15 +20,50 @@ def check_error(path, code, num, blank_line):
     if code.strip().startswith("class"):
         if re.match(r'class\s{2,}.*', code.strip()):
             print(f"{path}: Line {num}: S007 Too many spaces after 'class'")
-        class_name = code.lstrip("class").rstrip(":").strip("\n").lstrip()
-        if not re.match(r'[A-Z]', class_name):
-            print(f"{path}: Line {num}: S008 Class name '{class_name}' should use CamelCase")
+        for class_name in tree_result["class_names"]:
+            if class_name in code:
+                print(f"{path}: Line {num}: S008 Class name '{class_name}' should use CamelCase")
     if code.strip().startswith("def"):
         if re.match(r'def\s{2,}.*', code.strip()):
             print(f"{path}: Line {num}: S007 Too many spaces after 'def'")
-        func_name = code.lstrip("def").rstrip(":").strip("\n").lstrip()
-        if not re.match(r'[a-z_]', func_name):
-            print(f"{path}: Line {num}: S009 Function name '{func_name}' should use snake_case")
+        for func_name in tree_result["func_names"]:
+            if func_name in code:
+                print(f"{path}: Line {num}: S009 Function name '{func_name}' should use snake_case")
+        for func_arg in tree_result["func_args"]:
+            if func_arg in code:
+                print(f"{path}: Line {num}: S010 Argument name '{func_arg}' should be snake_case")
+        for func_name in tree_result["defaults"]:
+            if func_name in code:
+                print(f"{path}: Line {num}: S012 Default argument value is mutable")
+    for variable_name in tree_result["variable_names"]:
+        if variable_name in code and "=" in code:
+            print(f"{path}: Line {num}: S011 Variable '{variable_name}' in function should be snake_case")
+    # error_result += check_ast(code, path, num)
+    # error_result.sort()
+    # print(*error_result, sep="\n")
+
+
+def check_ast(tree):
+    tree_result = {"func_names": [], "func_args": [], "class_names": [], "variable_names": [], "defaults": []}
+    for node in ast.walk(tree):
+        if isinstance(node, ast.ClassDef):
+            class_name = node.name
+            if not re.match(r'[A-Z]', class_name):
+                tree_result["class_names"].append(class_name)
+        if isinstance(node, ast.FunctionDef):
+            func_name = node.name
+            if not re.match(r'[a-z_]', func_name):
+                tree_result["func_names"].append(func_name)
+            for func_arg in [a.arg for a in node.args.args]:
+                if not re.match(r'[a-z_]', func_arg):
+                    tree_result["func_args"].append(func_arg)
+            for dft_arg in node.args.defaults:
+                if isinstance(dft_arg, ast.List) or isinstance(dft_arg, ast.Set) or isinstance(dft_arg, ast.Dict):
+                    tree_result["defaults"].append(func_name)
+        if isinstance(node, ast.Name) and isinstance(node.ctx, ast.Store):
+            if not re.match(r'[a-z_]', node.id):
+                tree_result["variable_names"].append(node.id)
+    return tree_result
 
 
 def get_path():
@@ -43,6 +79,9 @@ def get_path():
 
 
 for file_path in get_path():
+    script = open(file_path).read()
+    code_tree = ast.parse(script)
+    tree_error = check_ast(code_tree)
     with open(file_path, 'r') as code_file:
         line_num = 0
         blank_lines = 0
@@ -51,5 +90,5 @@ for file_path in get_path():
             if line.strip("\n") == "":
                 blank_lines += 1
             else:
-                check_error(file_path, line, line_num, blank_lines)
+                check_error(file_path, line, line_num, blank_lines, tree_error)
                 blank_lines = 0
