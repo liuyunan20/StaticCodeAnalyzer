8f7a155e54fdc7b5ac9237f0acd1ff2812f14392
liuyunan <liuyunan.20@gmail.com>
2022-04-21 21:08:36 -0700

    Complete the code below. Build an AST of the expression, put all nodes in a list, and print the list length.

---
+++ Topics/ast module/A list of nodes/main.py
@@ -0,0 +1,10 @@
+import ast
+
+expression = "(34 + 6) * (23**2 - 7 + 45**2)"
+
+# put your code here
+tree = ast.parse(expression)
+nodes = []
+for node in ast.walk(tree):
+    nodes.append(node)
+print(len(nodes))
